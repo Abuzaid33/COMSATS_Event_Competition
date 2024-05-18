4e7e8f007539b2c17c6872fbdc0e4bda1d9d17ae
Abuzaid33 <abuzaidkhattak333@gmail.com>
2024-05-18 12:12:32 +0500

    Settingup project

--- comsats/comsats/settings.py
+++ comsats/comsats/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'user',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
