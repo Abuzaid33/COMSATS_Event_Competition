c612b78112ee0dfe157cf6176dae9e591db0fddd
Abuzaid33 <abuzaidkhattak333@gmail.com>
2024-05-18 14:47:29 +0500

    others

--- comsats/comsats/settings.py
+++ comsats/comsats/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'base',
     'user',
     'home',
+    'chatbot',
+    'ChatApp',
 ]
 
 MIDDLEWARE = [
--- comsats/comsats/urls.py
+++ comsats/comsats/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('user.urls')),
+    path('', include('chatbot.urls')),
     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
